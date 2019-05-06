40d0c9505cc330214f261928e93866bbecf2ef25
Gian Libunao <gianlibunao@gmail.com>
2019-05-06 20:35:26 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,29 +1,6 @@
 # Write your code here.
 def dictionary(word)
-  case word
-  when "hello"
-    "hi"
-  when "to"
-    "2"  
-  when "two"
-    "2"  
-  when "too"
-    "2"  
-  when "for"
-    "4"
-  when "four"
-    "4"
-  when "be"
-    "b"
-  when "you"
-    "u"
-  when "at"
-    "@"
-  when "and"
-    "&"
-  else
-  word
-end
+  
 end
 
 def word_substituter(inpString)
