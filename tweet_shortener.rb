afc35bf63a4a628d5a785602c2a984e89818692f
Gian Libunao <gianlibunao@gmail.com>
2019-05-06 20:50:39 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,13 +1,6 @@
 # Write your code here.
 def dictionary(word)
-  replacement = {
-    "hi" => "hello",
-    "2" => ["to","two","too"],
-    "4" => ["for","four"],
-    "b" => "be",
-    "u" => "you",
-    "@" => "at",
-    "&" => "and"
+  
   }
   
   
