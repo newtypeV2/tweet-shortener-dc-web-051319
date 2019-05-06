f32b89cb56659564186002b682eb5787d894a9a9
Gian Libunao <gianlibunao@gmail.com>
2019-05-06 20:05:20 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,18 @@
 # Write your code here.
 def dictionary(word)
   case word
-  
+  when "hello"
+    hi
+  when "to" || "two" || "too"
+    "2"
+  when "for" || "four"
+    "4"
+  when "be"
+    "b"
+  when "you"
+    "u"
+  when "at"
+    "@"
+  when "and"
+    "&"
 end
